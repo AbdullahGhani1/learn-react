389503c9b1c12b0d26be3592c71bd73404e01819
abdullahghani1 <engr.abdullahghani@gmail.com>
2023-04-13 02:44:58 +0500

    create user List

--- 5-user-data/src/App.tsx
+++ 5-user-data/src/App.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import "./App.css";
 import AddUsers from "./components/Users/AddUsers";
+import UsersList from "./components/Users/UsersList";
+interface User {
+  name: string;
+  age: number;
+  id: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const userAddHandler = (name: string, age: number) => {
+    setUsers((prevUsers) => {
+      return [...prevUsers, { name, age, id: Math.random().toString() }];
+    });
+  };
   return (
     <div className="App">
-      <AddUsers />
+      <AddUsers onAddUser={userAddHandler} />
+      <UsersList users={users} />
     </div>
   );
 }
--- 5-user-data/src/components/Users/AddUsers.tsx
+++ 5-user-data/src/components/Users/AddUsers.tsx
@@ -2,23 +2,51 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUsers.module.css";
 import Button from "../UI/Button";
-const AddUsers = () => {
-  const [enteredName, setEnteredName] = useState("");
-  const enteredNameChangeHandler = (
+interface AddUsersProps {
+  onAddUser: (name: string, age: number) => void;
+}
+const AddUsers = (props: AddUsersProps) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const userNameChangeHandler = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    setEnteredName(event.target.value);
+    setEnteredUserName(event.target.value);
   };
   const addUserHandler = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (enteredAge.trim().length === 0 || enteredUserName.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(`userName\t${enteredUserName}\nage\t${enteredAge}`);
+    props.onAddUser(enteredUserName, +enteredAge);
+
+    setEnteredUserName("");
+    setEnteredAge("");
+  };
+  const ageChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredAge(event.target.value);
   };
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text" onChange={enteredNameChangeHandler} />
+        <input
+          id="username"
+          value={enteredUserName}
+          type="text"
+          onChange={userNameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
