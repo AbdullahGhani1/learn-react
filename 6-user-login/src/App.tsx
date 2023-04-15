693bfb661d49f75894af2c61197e8fdcee9aeaac
abdullahghani1 <engr.abdullahghani@gmail.com>
2023-04-16 03:34:38 +0500

    creating header and Home Component

--- 6-user-login/src/App.tsx
+++ 6-user-login/src/App.tsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import MainHeader from "./components/MainHeader/MainHeader";
+import Home from "./components/Home/Home";
 
 function App() {
-  return <div className="App">dev</div>;
+  return (
+    <React.Fragment>
+      <MainHeader />{" "}
+      <main>
+        <Home />
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ 6-user-login/src/components/UI/Card/Card.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Card.module.css";
+interface CardProps {
+  className?: string;
+  children: React.ReactNode;
+}
+const Card = (props: CardProps) => {
+  return (
+    <div className={`${classes.card} ${props.className} `}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
