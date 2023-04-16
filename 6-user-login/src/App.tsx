122c0a74dbb0436219effb72ea0c799089a914b5
abdullahghani1 <engr.abdullahghani@gmail.com>
2023-04-17 03:29:11 +0500

    useEffect localstoraage

--- 6-user-login/src/App.tsx
+++ 6-user-login/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import MainHeader from "./components/MainHeader/MainHeader";
 import Home from "./components/Home/Home";
@@ -6,12 +6,19 @@ import Login from "./components/Login/Login";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  useEffect(() => {
+    const storeUserLoginStatus = localStorage.getItem("isLoggedIn");
+    if (storeUserLoginStatus === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
   const loginHandler = (email: string, password: string) => {
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
