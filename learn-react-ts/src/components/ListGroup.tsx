740682e865d1948cad0fdb98b24f99287368ed10
abdullahghani1 <engr.abdullahghani@gmail.com>
2025-06-03 21:38:55 +0500

    event Handler

--- learn-react-ts/src/components/ListGroup.tsx
+++ learn-react-ts/src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import React, { type MouseEvent } from "react";
+
 function ListGroup() {
   const ohioCities = [
     "Cleveland",
@@ -11,13 +13,20 @@ function ListGroup() {
     "Lakewood",
     "Youngstown",
   ];
+  // Event Handler
+  const onHandleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h1 className="display-1">List</h1>
       {ohioCities.length === 0 && <p className="display-6">No City Found</p>}
       <ul className="list-group">
         {ohioCities.map((city, index) => (
-          <li className="list-group-item" key={index}>
+          <li
+            className="list-group-item cursor-grab active"
+            key={index}
+            onClick={onHandleClick}
+          >
             {city}
           </li>
         ))}
