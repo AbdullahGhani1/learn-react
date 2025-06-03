8e48c36180babbcfb79e6d34723f29c8703c35d5
abdullahghani1 <engr.abdullahghani@gmail.com>
2025-06-03 16:02:36 +0500

    initialize react app and create an message component

---
+++ learn-react-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Message from "./components/Message";
+
+function App() {
+  return (
+    <main>
+      <Message />
+    </main>
+  );
+}
+
+export default App;
---
+++ learn-react-ts/src/components/Message.tsx
@@ -0,0 +1,8 @@
+// PascalCasing
+function Message() {
+  const name = "Abdullah Ghani";
+  // JSX: JavaScript XML
+  return <h1>Hello {name}</h1>;
+}
+
+export default Message;
