234b947276b125a1fe82b17baad8da97620827b7
abdullahghani1 <engr.abdullahghani@gmail.com>
2025-06-03 17:33:51 +0500

    adding bootstrap

--- learn-react-ts/src/App.tsx
+++ learn-react-ts/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ListGroup from "./components/ListGroup";
 import Message from "./components/Message";
 
 function App() {
   return (
     <main>
       <Message />
+      <ListGroup />
     </main>
   );
 }
