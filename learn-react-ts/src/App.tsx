7fc9f6b2505259e0e8ff36ce130cd0e651f5e2ce
abdullahghani1 <engr.abdullahghani@gmail.com>
2025-06-04 01:52:36 +0500

    props passing

--- learn-react-ts/src/App.tsx
+++ learn-react-ts/src/App.tsx
@@ -3,10 +3,29 @@ import ListGroup from "./components/ListGroup";
 import Message from "./components/Message";
 
 function App() {
+  const ohioCities = [
+    "Cleveland",
+    "Toledo",
+    "Akron",
+    "Lorain",
+    "Elyria",
+    "Sandusky",
+    "Mentor",
+    "Parma",
+    "Lakewood",
+    "Youngstown",
+  ];
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <main>
       <Message />
-      <ListGroup />
+      <ListGroup
+        items={ohioCities}
+        heading="Cities"
+        onSelectedItem={handleSelectedItem}
+      />
     </main>
   );
 }
--- learn-react-ts/src/components/ListGroup.tsx
+++ learn-react-ts/src/components/ListGroup.tsx
@@ -1,33 +1,32 @@
-import React, { type MouseEvent } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  const ohioCities = [
-    "Cleveland",
-    "Toledo",
-    "Akron",
-    "Lorain",
-    "Elyria",
-    "Sandusky",
-    "Mentor",
-    "Parma",
-    "Lakewood",
-    "Youngstown",
-  ];
-  // Event Handler
-  const onHandleClick = (event: MouseEvent) => console.log(event);
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectedItem: (item: string) => void;
+}
+function ListGroup({ items, heading, onSelectedItem }: ListGroupProps) {
+  const [selectedCity, setSelectedCity] = useState(-1);
 
   return (
     <>
-      <h1 className="display-1">List</h1>
-      {ohioCities.length === 0 && <p className="display-6">No City Found</p>}
+      <h1 className="display-1">{heading}</h1>
+      {items.length === 0 && <p className="display-6">No City Found</p>}
       <ul className="list-group">
-        {ohioCities.map((city, index) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item cursor-grab active"
+            className={
+              selectedCity === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={index}
-            onClick={onHandleClick}
+            onClick={() => {
+              setSelectedCity(index);
+              onSelectedItem(item);
+            }}
           >
-            {city}
+            {item}
           </li>
         ))}
       </ul>
