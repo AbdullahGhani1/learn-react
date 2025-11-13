5cd84f3ecc407f45cd172a43d054de5a5337d24e
abdullahghani1 <engr.abdullahghani@gmail.com>
2025-11-13 10:09:55 +0500

    responsive Design

--- projects/learn-react-ts/game-grid/src/App.tsx
+++ projects/learn-react-ts/game-grid/src/App.tsx
@@ -1,13 +1,25 @@
-import { Button, Heading, HStack } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <HStack>
-        <Heading>Game Grid</Heading>
-        <Button>Click Me</Button>
-      </HStack>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      gap="4"
+      padding="4"
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
